284709a699ee8035842b1493b5cdeb6ca3a6e819
Alex <alxselby@gmail.com>
2023-07-31 12:53:09 -0400

    tested

--- dynamic_permissions.sql
+++ dynamic_permissions.sql
@@ -3,15 +3,14 @@ RETURNS event_trigger AS
 $$
 BEGIN
   -- change these to your role and schema
-  EXECUTE format('GRANT SELECT ON ALL TABLES IN SCHEMA public TO analyst');
-  EXECUTE format('GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA public TO analyst');
+  EXECUTE format('GRANT SELECT ON ALL TABLES IN SCHEMA public TO analyst;');
+  EXECUTE format('GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA public TO analyst;');
 END;
 $$
 LANGUAGE plpgsql;
 
-
+DROP EVENT TRIGGER IF EXISTS trigger_grant_permissions;
 CREATE EVENT TRIGGER trigger_grant_permissions
 ON ddl_command_end
-WHEN TAG IN ('CREATE TABLE', 'ALTER TABLE')
+WHEN TAG IN ('CREATE TABLE', 'ALTER TABLE', 'DROP TABLE')
 EXECUTE FUNCTION grant_permissions();
-
