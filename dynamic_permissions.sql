bc7805a88f1a7fc65635615c1950d5c0082dceb0
Alex <alxselby@gmail.com>
2023-07-31 12:12:47 -0400

    first commit

---
+++ dynamic_permissions.sql
@@ -0,0 +1,16 @@
+CREATE OR REPLACE FUNCTION grant_permissions()
+RETURNS event_trigger AS
+$$
+BEGIN
+  -- change these to your role and schema
+  EXECUTE format('GRANT SELECT ALL TABLES IN SCHEMA public TO analyst');
+  EXECUTE format('GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA public TO analyst');
+END;
+$$
+LANGUAGE plpgsql;
+
+
+CREATE EVENT TRIGGER trigger_grant_permissions
+ON ddl_command_end
+WHEN TAG IN ('CREATE TABLE', 'ALTER TABLE')
+EXECUTE FUNCTION grant_permissions();
