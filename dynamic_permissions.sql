b43d641640379a5817a00c31063b24cafe80ce1d
Alex <alxselby@gmail.com>
2023-07-31 12:16:45 -0400

    fixing item

--- dynamic_permissions.sql
+++ dynamic_permissions.sql
@@ -3,7 +3,7 @@ RETURNS event_trigger AS
 $$
 BEGIN
   -- change these to your role and schema
-  EXECUTE format('GRANT SELECT ALL TABLES IN SCHEMA public TO analyst');
+  EXECUTE format('GRANT SELECT ON ALL TABLES IN SCHEMA public TO analyst');
   EXECUTE format('GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA public TO analyst');
 END;
 $$
@@ -14,3 +14,4 @@ CREATE EVENT TRIGGER trigger_grant_permissions
 ON ddl_command_end
 WHEN TAG IN ('CREATE TABLE', 'ALTER TABLE')
 EXECUTE FUNCTION grant_permissions();
+
